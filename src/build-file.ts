3829a4b32046e5ee01b413fc9311a9ff4d8288a5
PurpleDragonfly <10940221@qq.com>
2021-05-27 10:18:05 +0800

    update

--- src/build-file.ts
+++ src/build-file.ts
@@ -21,8 +21,8 @@ class BuildFile {
   async createModule(entryPath: string, entry: string) {
     const fileText = await Deno.readTextFile(entryPath);
     const fileSource: string = fileText.toString();
-    const { code, deps } = this.resourceParse(fileSource, path.dirname(entry));
-    //读取的内容加入到modules对象中
+    this.resourceParse(fileSource, path.dirname(entry));
+    //读取的内容加入到modules对象中const { code, deps } =
     BuildFile.modules[entry] = code;
 
     for (let i in deps) {
